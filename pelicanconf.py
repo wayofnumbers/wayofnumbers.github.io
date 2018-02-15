88632da075028351cd71876ac59f9bcedd9146cb
Michael Li <excalibarlee@yahoo.com>
2018-02-14 22:03:31 -0600

    d

--- pelicanconf.py
+++ pelicanconf.py
@@ -102,9 +102,10 @@ THEME = "/home/lisper/pelican-themes/pelican-chameleon"
 #        BS3_THEME = ('/3rdparty/bootswatch/%s/bootstrap.min.css'
 #                     % _BS3_THEME_NAME)
 
-BS3_THEME = 'https://bootswatch.com/4/cosmo/bootstrap.min.css'
+BS3_THEME = 'https://bootswatch.com/4/flatly/bootstrap.min.css'
 # BS3_JS = '/home/lisper/Downloads/jupyter-blog/3rdparty/bootstrap/3.0.0/js/bootstrap.min.js'
-BS3_URL = 'https://bootswatch.com/4/cosmo/bootstrap.min.css'
+BS3_URL = 'https://bootswatch.com/4/flatly/bootstrap.min.css'
+BS3_JS  = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js'
 # JQUERY_JS = '/home/lisper/Downloads/jupyter-blog/3rdparty/jquery/jquery-1.10.1.min.js'
 # JQUERY_MIGRATE_JS = '/home/lisper/Downloads/jupyter-blog/3rdparty/jquery/jquery-migrate-1.2.1.min.js'
 
