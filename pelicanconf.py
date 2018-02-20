5aff40d3d7efe53abb2b45c0d951696c53dd218b
Michael Li <excalibarlee@yahoo.com>
2018-02-20 10:47:02 -0600

    fix

--- pelicanconf.py
+++ pelicanconf.py
@@ -184,7 +184,7 @@ TAG_SAVE_AS = ''
 CATEGORY_SAVE_AS = ''
 AUTHOR_SAVE_AS = ''
 LANDING_PAGE_ABOUT = {'title': "Welcome!", 
-		      'details' : "We are a small team of researchers, designers, developers that likes to explore. Data science don't have to be boring, and we want to help"
+		      'details' : "We are a small team of researchers, designers, developers that likes to explore. Data science don't have to be boring, and we are here to help."
 			}
 PROJECTS = [{
     'name': 'Mucraft.net',
