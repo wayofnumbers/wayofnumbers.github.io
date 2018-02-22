1b6e0ea303667b5dbe4b928559dba6b0e650e8f8
Michael Li <excalibarlee@yahoo.com>
2018-02-22 10:33:25 -0600

    fix

--- plugins/readtime/readtime.py
+++ plugins/readtime/readtime.py
@@ -1,19 +1,16 @@
+import urllib
 import bs4
-import urllib, re
 from pelican import signals
 
 def extract_text(url):
+    '''extract texts from url'''
     html = urllib.request.urlopen(url).read()
     soup = bs4.BeautifulSoup(html, 'html.parser')
     texts = soup.findAll(text=True)
     return texts
 
-
-def register():
-    signals.content_object_init.connect(calculate_readtime)
-
-
 def is_visible(element):
+    '''tell whether text has tags'''
     if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
         return False
     elif isinstance(element, bs4.element.Comment):
@@ -24,32 +21,35 @@ def is_visible(element):
 
 
 def filter_visible_text(page_texts):
+    '''remove tags etc.'''
     return filter(is_visible, page_texts)
 
 WPM = 200
 WORD_LENGTH = 5
 
 def count_words_in_text(text_list, word_length):
+    '''count words'''
     total_words = 0
     for current_text in text_list:
         total_words += len(current_text)/word_length
     return total_words
 
 def estimate_reading_time(content_object):
+    '''estimate the reading time of an article'''
     if content_object._content is not None:
         content = content_object._content
 
         filtered_text = filter_visible_text(content)
         total_words = count_words_in_text(filtered_text, WORD_LENGTH)
-
         minutes = int(math.ceil(total_words/WPM))
         if minutes == 0:
             minutes =1
 
         content_object.readtime = {
-	    "minutes": minutes,
+            "minutes": minutes,
         }
 
 
 def register():
+    '''api'''
     signals.content_object_init.connect(estimate_reading_time)
