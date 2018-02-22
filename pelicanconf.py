35795ebe4127714f9eb501cddc0aa36e87b20b84
Michael Li <excalibarlee@yahoo.com>
2018-02-22 08:35:35 -0600

    fix

--- pelicanconf.py
+++ pelicanconf.py
@@ -52,7 +52,7 @@ MARKUP = ('md', 'ipynb')
 # STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico', 'extra/logo.svg']
 MENUITEMS = [('Homepage', '/'),('Categories','/categories.html'), ('About', './pages/about.html')]
 PLUGIN_PATHS= ['./plugins']
-PLUGINS = ['ipynb.markup','sitemap', 'extract_toc', 'neighbors', 'tipue_search']
+PLUGINS = ['ipynb.markup','sitemap', 'extract_toc', 'neighbors', 'tipue_search', 'readtime']
 
 TYPOGRIFY = True
 
